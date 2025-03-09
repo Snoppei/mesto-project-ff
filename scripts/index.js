0ebfa9a5c4b3ecdf5d5fc22bf0d2b509201245a8
Snoppei <negulyaev2017@gmail.com>
2025-03-09 19:42:19 +0300

    add: removing and adding cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function addCard(cardProperties, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+    cardImage.src = cardProperties.link;
+    cardImage.alt = 'Фотография, ' + cardProperties.name;
+    cardTitle.textContent = cardProperties.name;
 
-// @todo: Вывести карточки на страницу
+    cardDeleteButton.addEventListener('click', function () {
+        deleteCard(cardElement);
+    })
+
+    placesList.append(cardElement);
+}
+
+function deleteCard(card) {
+    card.remove();
+}
+
+initialCards.forEach(function (element) {
+    addCard(element, deleteCard);
+});
