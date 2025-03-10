e1f36d325f8db3ab15460e4b8e660133fb34f1de
Snoppei <negulyaev2017@gmail.com>
2025-03-10 11:40:23 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -2,23 +2,29 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 const placesList = document.querySelector('.places__list');
 
-function addCard(cardProperties, deleteCard) {
+function createCard(cardProperties, deleteCard) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const cardImage = cardElement.querySelector('.card__image');
     const cardTitle = cardElement.querySelector('.card__title');
     const cardDeleteButton = cardElement.querySelector('.card__delete-button');
 
     cardImage.src = cardProperties.link;
-    cardImage.alt = 'Фотография, ' + cardProperties.name;
+    cardImage.alt = cardProperties.name;
     cardTitle.textContent = cardProperties.name;
 
     cardDeleteButton.addEventListener('click', () => deleteCard(cardElement));
 
-    placesList.append(cardElement);
+    return cardElement;
 }
+
+function addCard(card, targetNode) {
+    targetNode.append(card);
+};
 
 function deleteCard(card) {
     card.remove();
 }
 
-initialCards.forEach(element => addCard(element, deleteCard));
+initialCards.forEach(element => {
+    addCard(createCard(element, deleteCard), placesList);
+});
